93e0c736fcfea06dfec55331222a11eb017cac52
Sasha Varlamov <sasha@sashavarlamov.com>
2016-05-21 23:59:32 +0800

    Add restaurant suggestions

--- app/src/main/java/com/finder/fooedbar/client/api/Restaurant.java
+++ app/src/main/java/com/finder/fooedbar/client/api/Restaurant.java
@@ -7,33 +7,36 @@ import org.json.JSONObject;
  */
 
 public class Restaurant {
-    private String id;
+    private int id;
     private String name;
     private String description;
     private String style;
-
-    public String getId() {
-        return id;
-    }
-
-    public String getName() {
-        return name;
-    }
-
-    public String getDescription() {
-        return description;
-    }
-
-    public String getStyle() {
-        return style;
-    }
+    private String imageUrl;
+    private int imageHeight;
+    private int imageWidth;
+    private int sessionId;
 
     public Restaurant(int id, int sessionId) throws Exception {
         JsonHttpUtils utils = new JsonHttpUtils(sessionId);
-        this.id = ((Integer)id).toString();
-        JSONObject obj = utils.makeJsonGetRequest("v0/restaurants/" + this.id);
+        this.id = id;
+        JSONObject obj = utils.makeJsonGetRequest("v0/restaurants/" + ((Integer)this.id).toString());
+        this.sessionId = sessionId;
         this.name = obj.getString("name");
         this.description = obj.getString("description");
         this.style = obj.getString("style");
+        this.imageUrl = obj.getString("imageUrl");
+        this.imageHeight = obj.getInt("imageHeight");
+        this.imageWidth = obj.getInt("imageWidth");
+    }
+
+    public Restaurant(int id, String name, String description, String style, String imageUrl, int imageHeight, int imageWidth, int sessionId) {
+        this.id = id;
+        this.name = name;
+        this.description = description;
+        this.style = style;
+        this.imageHeight = imageHeight;
+        this.imageUrl = imageUrl;
+        this.imageWidth = imageWidth;
+        this.sessionId = sessionId;
     }
 }
---
+++ app/src/main/java/com/finder/fooedbar/client/api/RestaurantSuggestions.java
@@ -0,0 +1,41 @@
+package com.finder.fooedbar.client.api;
+
+import org.json.JSONArray;
+import org.json.JSONObject;
+
+import java.util.ArrayList;
+
+/**
+ * Created by svarlamov on 21/5/2016.
+ */
+
+public class RestaurantSuggestions {
+    private ArrayList<Restaurant> restaurants = new ArrayList<>();
+    private int sessionId;
+    private JsonHttpUtils utils;
+
+    public RestaurantSuggestions(int sessionId) {
+        this.sessionId = sessionId;
+        utils = new JsonHttpUtils(this.sessionId);
+    }
+
+    public void getSuggestions() throws Exception {
+        JSONObject resp = utils.makeJsonGetRequest("v0/suggestions/restaurants");
+        JSONArray items = resp.getJSONArray("items");
+        for (int i = 0; i < items.length(); i++) {
+            JSONObject obj = items.getJSONObject(i);
+            this.restaurants.add(new Restaurant(obj.getInt("id"),
+                    obj.getString("name"), obj.getString("description"),
+                    obj.getString("style"), obj.getString("imageUrl"),
+                    obj.getInt("imageHeight"), obj.getInt("imageWidth"), this.sessionId));
+        }
+    }
+
+    public int size() {
+        return this.restaurants.size();
+    }
+
+    public Restaurant get(int index) {
+        return this.restaurants.get(index);
+    }
+}
